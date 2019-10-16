63529f386396a75276960f9626ea7f3166277444
Monika <moni.kesarwani@yahoo.com>
2019-10-16 18:31:59 -0400

    add textfield within UIAlert and UIAction

--- todo App/TodoListViewController.swift
+++ todo App/TodoListViewController.swift
@@ -9,9 +9,9 @@
 /*
  1. conform the UITableViewController protocol
  2. rename ViewController accordingly
- 3.delete the viewcontroller from mainstory board and than add UITableViewController
+ 3. delete the viewcontroller from mainstory board and than add UITableViewController
  4. link the UITableViewController to todolistViewController class through identity inspector
- 5.select prototype cell and through attribute inspector mention cell name (identifer name)
+ 5. select prototype cell and through attribute inspector mention cell name (identifer name)
  6. than use tabelview datasource and delegates methods
  */
 
@@ -19,7 +19,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    //creating array for hardcore data
+    //creating array for saving hardcore data
     var itemArray = ["Shopping", "Cleaning", "Cooking"]
 
     override func viewDidLoad() {
@@ -29,21 +29,24 @@ class TodoListViewController: UITableViewController {
 
     //MARK - TabelView Datasource Methods
     
+    //Declare numberOfRowsInSection here:
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
 
-     
+     //Declare cellForRowAtIndexPath here:
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
     
-    //MARK - TabelView Delegate Methods
     
+    //MARK - TabelView Delegate Methods
+    //Declare didSelectRowAt  here:
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         print(itemArray[indexPath.row])
         //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         tableView.deselectRow(at: indexPath, animated: true)
@@ -58,6 +61,30 @@ class TodoListViewController: UITableViewController {
     
     
     
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+       
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item in Todoey List", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            print("Success!")
+            
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+            
+        }
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in
+        alertTextField.placeholder = "Create new item"
+        
+        textField = alertTextField
+                
+        }
+
+
+        present(alert, animated: true, completion: nil)
+    }
     
 }
 
